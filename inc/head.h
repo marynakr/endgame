d3d78746c464b2b3819f737cf42a0f8919ba8f8d
Oleg Berdyshev <oberdyshev@e2r5p9.unit.ua>
2019-09-30 19:20:37 +0300

    1

---
+++ inc/head.h
@@ -0,0 +1,16 @@
+#ifndef HEAD_H
+#define HEAD_H
+
+#include <unistd.h>
+#include <SDL2/SDL.h>
+#include <SDL2_image/SDL_image.h>
+//#include <SDL2/SDL_image.h>
+#include <SDL2_ttf/SDL_ttf.h>
+#include <SDL2/SDL_timer.h>
+
+void mx_printerr(const char *s);
+int mx_strlen(const char *s);
+
+
+#endif
+
